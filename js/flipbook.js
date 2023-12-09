afc7091d72c9586d7362a74ae15f95eb97719431
Miguel Gomez <himiggu@gmail.com>
2023-12-08 23:10:18 -0300

    Future Preparations for a ticker change when changing Symbol

--- js/flipbook.js
+++ js/flipbook.js
@@ -1,16 +1,16 @@
-const defaultOptions = {
-  start: 0, //start frame
-  end: 100, //end frame, must be greater then start
-  step: 1, //number of frames to step over while animating
-  mobileStep: 3, //number of frames to step over when on a mobile device
-  fps: 15, //frames per second, this will be adjusted correctly for step>1
-  loop: false, //loop the animation
-  //the image path, uses %d to designate where the frame number goes.
-  //%#d can be used to specify padding
-  images: "",
-};
-
 function flipbook(img, opts) {
+  const defaultOptions = {
+    start: 0, //start frame
+    end: 100, //end frame, must be greater then start
+    step: 1, //number of frames to step over while animating
+    mobileStep: 3, //number of frames to step over when on a mobile device
+    fps: 15, //frames per second, this will be adjusted correctly for step>1
+    loop: false, //loop the animation
+    //the image path, uses %d to designate where the frame number goes.
+    //%#d can be used to specify padding
+    images: "",
+  };
+
   const options = { ...defaultOptions, ...opts };
 
   const animatingAttr = "flipbook_animating";
